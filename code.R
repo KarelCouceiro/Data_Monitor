f8f29540689e371db3b3b3d3fa4ca224e58d2aa7
Karel Couceiro <karel.couceiro@gmail.com>
2022-11-23 21:13:57 +0100

    First web

--- code.R
+++ code.R
@@ -1,2 +1,7 @@
-library(GetFREData)
+library(fredr)
+library(forecast)
+fredr_set_key("ed43ed5f8fa6abd9fb6236a0f64445f5")
 
+#Balance of payments BPM6: Current account Balance: Total: Total Balance as % of GDP for the Czech Republic
+
+dat<-fredr(series_id = "CZEB6BLTT02STSAQ")
